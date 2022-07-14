2af4606d218f7876c6d990c9fbcd08beac7aeb7f
Moshe Praver <moshepraver@gmail.com>
2022-07-14 14:24:23 -0500

    maven init

---
+++ src/main/java/moshe/dev/moshepetclinic/MoshePetClinicApplication.java
@@ -0,0 +1,13 @@
+package moshe.dev.moshepetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoshePetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoshePetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/moshe/dev/moshepetclinic/MoshePetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package moshe.dev.moshepetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoshePetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
