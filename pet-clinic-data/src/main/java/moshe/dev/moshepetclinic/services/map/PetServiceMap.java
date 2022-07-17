0e726ecd4802e2b00ff4a2fb807b04708a2f40fe
Moshe Praver <moshepraver@gmail.com>
2022-07-17 15:36:36 -0500

    add controller/index.html

---
+++ pet-clinic-data/src/main/java/moshe/dev/moshepetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package moshe.dev.moshepetclinic.services.map;
+
+import moshe.dev.moshepetclinic.model.Pet;
+import moshe.dev.moshepetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById((id));
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete((object));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById((id));
+    }
+}
---
+++ pet-clinic-data/src/main/java/moshe/dev/moshepetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package moshe.dev.moshepetclinic.services.map;
+
+import moshe.dev.moshepetclinic.model.Vet;
+import moshe.dev.moshepetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById((id));
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete((object));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById((id));
+    }
+}
